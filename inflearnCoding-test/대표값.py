d331bc8e06905a6c3b343a24d638b7199d850602
githun DOHAN25 <github joke3323@naver.com>
2022-05-04 21:31:31 +0900

    대표값

---
+++ inflearnCoding-test/대표값.py
@@ -0,0 +1,25 @@
+import math
+students = int(input())
+
+students_grade = list(map(int, input().split()))
+
+ave = round(sum(students_grade)/students)
+
+min = float('inf')
+
+
+for idx, x in enumerate(students_grade):
+    tmp = abs(x-ave) # 절대값으로
+    if tmp < min:
+        min = tmp
+        socre = x
+        res = idx + 1
+    elif tmp == min:
+        if x > socre:
+            score = x
+            res = idx + 1
+
+
+print(ave, res)
+
+
