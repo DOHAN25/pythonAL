03d5bb28bbbeed8ee41ec192be959e37f48c7d02
githun DOHAN25 <github joke3323@naver.com>
2022-05-06 13:44:00 +0900

    정다면체

--- inflearnCoding-test/정다면체.py
+++ inflearnCoding-test/정다면체.py
@@ -1,19 +1,25 @@
 
 n, m = map(int, input("N과 M입력: ").split())
 
-result = list(range(2, n+m+2))
-
-print(result)
-
+result = [0]*(n+m+3)
+max=0
 for i in range(1, n+1):
-    for j in range(1,  m+1):
-        print(i, j)
+    for j in range(1, m+1):
+        result[i+j] += 1
+
+for i in range(n+m+1):
+    if result[i]>max:
+        max=result[i]
+
+for i in range(n+m+1):
+    if result[i]==max:
+        print(i, end=' ')
 
 
 
-result.sort()
 
-print(result[range(result)])
+
+
 
 
 
