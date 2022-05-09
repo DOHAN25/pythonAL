d4618ec7ce12e42136fa08e1cfe1f0874058401d
githun DOHAN25 <github joke3323@naver.com>
2022-05-09 21:00:49 +0900

    뒤집어진 소수 구하기

---
+++ inflearnCoding-test/뒤집어진소수.py
@@ -0,0 +1,28 @@
+
+
+num_list=list(input("숫자를 입력해주세요. : ").split())
+
+
+def reverse(list):
+    reverse_num_list=[]
+    for x in list:
+        reverse_num_list.append(int(x[::-1]))
+    return reverse_num_list
+
+def isPrime(x):
+    if x==1:
+        return False
+    for i in range(2, x//2+1):
+        if x%i==0:
+            return False
+    else:
+        return True
+
+
+
+
+
+for x in reverse(num_list):
+    if isPrime(x):
+        print(x, end=" ")
+
