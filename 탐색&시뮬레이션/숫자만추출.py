34b2efd4a7b7ec41beee35e8721272fcb9601d82
githun DOHAN25 <joke3323@naver.com>
2022-05-10 22:31:28 +0900

    숫자만추출 다른풀이

--- 탐색&시뮬레이션/숫자만추출.py
+++ 탐색&시뮬레이션/숫자만추출.py
@@ -19,3 +19,18 @@ for i in range(1, str+1):
         cnt+=1
 
 print(cnt)
+
+
+# 다른 풀이법
+# isdecimal : 0~9까지의 숫자인지 확인해준다 포함되면 : True
+for x in input_str:
+    if x.isdecimal():
+        res=res*10+int(x)
+
+print(x)
+
+cnt=0
+for i in range(1, res+1):
+    if res%i==0:
+        cnt+=1
+print(cnt)
