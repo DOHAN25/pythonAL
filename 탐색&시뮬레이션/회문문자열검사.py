2a5f4fd661b1feaf43b7f2ef4c88542a03e27985
githun DOHAN25 <joke3323@naver.com>
2022-05-12 23:12:49 +0900

    회문 문자열 재풀이

--- 탐색&시뮬레이션/회문문자열검사.py
+++ 탐색&시뮬레이션/회문문자열검사.py
@@ -27,3 +27,23 @@ for i in range(num):
 
 
 
+
+
+
+'''
+회문 문자열 
+n개의 문자열 데이터를 입력받아 앞에서 읽을 때나 뒤에서 읽을 때나 같은 경우(회문 문자열)이면 Yes
+를 출력하고 회문 문자열이 아니면 No를 출력하는 프로그램을 작성하여라.
+단 회문을 검사할때 대소문자를 구분하지 않는다.
+'''
+
+n=int(input("n:"))
+for i in range(n):
+    str=input("회문 문자열?:")
+    str.upper()
+    for j in range(len(str)//2):
+        if str[j]!=str[-1-j]:
+            print("%d No", %(i+1))
+            break
+    else:
+        print("%d Yes", %(i+1))
