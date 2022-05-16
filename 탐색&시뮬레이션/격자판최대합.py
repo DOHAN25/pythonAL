3136bae6d56b1dadccff5150d9f9e976ad6d3929
githun DOHAN25 <joke3323@naver.com>
2022-05-16 21:19:14 +0900

    격자판최대합

---
+++ 탐색&시뮬레이션/격자판최대합.py
@@ -0,0 +1,34 @@
+
+
+'''
+N*N 격자판이 주어진다면 각 행의 합, 각 열의 합, 두 대각선의 합 중 가장 큰 합을 출력하는 프로그램을
+작성하여라.
+'''
+
+n=int(input())
+a=[list(map(int, input().split())) for _ in range(n)]
+
+largest=-2147000000
+
+for i in range(n):
+    sum1=sum2=0
+    for j in range(n):
+        sum1+=a[i][j]
+        sum2+=a[j][i]
+    if sum1>largest:
+        largest=sum1
+    if sum2>largest:
+        largest=sum2
+
+sum1=sum2=0
+for i in range(n):
+    sum1+=a[i][i]
+    sum2+=a[i][n-i-1]
+    if sum1>largest:
+        largest=sum1
+    if sum2>largest:
+        largest=sum2
+
+print(largest)
+
+
