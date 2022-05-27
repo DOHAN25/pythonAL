2c5e0a2f691bd38b640febcce4e2881f7503b428
githun DOHAN25 <joke3323@naver.com>
2022-05-27 20:01:31 +0900

    스도쿠 수정

--- 탐색&시뮬레이션/스도쿠.py
+++ 탐색&시뮬레이션/스도쿠.py
@@ -8,4 +8,31 @@
 '''
 
 
-스도쿠=[list]
+input_list=[list(map(int, input(":").split())) for _ in range(9)]
+
+
+check_row = [0]*10
+check_column = [0]*10
+check_diagnal = [0]*10
+result=0
+
+def check_list(list):
+    for i in range(9):
+        for j in range(9):
+            check_row[input_list[i][j]]=1
+            check_column[input_list[j][i]]=1
+        if sum(check_row)!=9 or sum(check_column)!=9:
+            return False
+    for i in range(3):
+        for j in range(3):
+            for x in range(3):
+                for y in range(3):
+                    check_diagnal[input_list[i*3+x][j*3+y]]=1
+            if sum(check_diagnal)!=9:
+                return False
+    return True
+
+if check_list(input_list):
+    print("Yes")
+else:
+    print("No")
